aaa8ec4a13a8e262eec6d75d914acf85f9b5a56c
Deepak Jaiswal <jaiswaldj96@gmail.com>
2021-04-10 23:25:41 +0530

    [Deepak] modified method to operate on floats

--- src/FindMax.java
+++ src/FindMax.java
@@ -10,4 +10,11 @@ public class FindMax {
             return secondNumber;
         return thirdNumber;
     }
+    public  Double findMaximum(Double firstNumber, Double secondNUmber, Double thirdNumber){
+        if(firstNumber.compareTo(secondNUmber) > 0.0 && firstNumber.compareTo(thirdNumber) > 0.0)
+            return firstNumber;
+        if(secondNUmber.compareTo(thirdNumber) > 0.0)
+            return secondNUmber;
+        return thirdNumber;
+    }
 }
